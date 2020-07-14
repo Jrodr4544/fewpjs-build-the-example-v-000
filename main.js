7780218e53af2abb6cb2dd922deacb5be43201ee
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-07-14 19:46:43 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+let liked = false
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const likeBtn = document.querySelector(".like")
+  const errorDiv = document.getElementById("modal")
 
+  likeBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    debugger
+    mimicServerCall()
+      .then(() => {
+        if (likeBtn.className == "activated-heart") {
+          likeBtn.innerHTML = `Like! ${EMPTY_HEART}`
+          likeBtn.className = "deactivated-heart"
+        } else {
+          likeBtn.innerHTML = FULL_HEART
+          likeBtn.className = "activated-heart"
+        }
+      })
+      .catch((error) => {
+        errorDiv.className = "unhidden"
+        errorDiv.innerText = error
+        setTimeout(() => {
+          errorDiv.className = "hidden"
+        }, 5000)
+      })
+
+  });
+})
 
 
 
